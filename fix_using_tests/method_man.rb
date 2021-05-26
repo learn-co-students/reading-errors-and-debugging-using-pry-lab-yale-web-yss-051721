a7ddf7cbd80b3d8c8aac070c325a674e8c73a277
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-26 20:12:42 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
 
-def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
-end
+require 'pry'
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def start_game(player1, player2)
+  return "Hello #{player1} & #{player2}"
 end
+start_game("Rocky","Bullwinkle")
+
+
+
+def play_game(player1, player2)
+  return "#{player1} is better than #{player2}" 
+end
+play_game("Jay-Z", "Beyonce")
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string[0] = "s"
+     return 9.times "s"
+     return string
   else
     string
   end
